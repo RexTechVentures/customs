d611109edeedcb078e95bcfb7cad70de41514e98
Rein Oterholm <rein@otter.productions>
2022-05-24 12:24:41 -0500

    Remove dist and add custom release command

--- dist/src/entity.d.ts
+++
@@ -1,9 +0,0 @@
-export interface EntityReference {
-    id: string;
-    type: string;
-}
-export declare const refEq: (ref1: EntityReference, ref2: EntityReference) => boolean;
-export interface EntityProvider {
-    getReference(entity: any): Promise<EntityReference>;
-    getParent(entityReference: EntityReference): Promise<EntityReference | undefined>;
-}
--- dist/src/entity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.refEq = void 0;
-const refEq = (ref1, ref2) => ref1.id === ref2.id && ref1.type === ref2.type;
-exports.refEq = refEq;
-//# sourceMappingURL=entity.js.map
